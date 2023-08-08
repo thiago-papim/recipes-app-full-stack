fc536547d456021979026bf83b0737aaacb5a1fb
Larissa Simões <larissacarneiro2@hotmail.com>
2023-08-08 10:06:32 -0300

    Configuração inicial

---
+++ app/backend/build/app.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = exports.App = void 0;
+const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.config();
+        this.app.get('/', (req, res) => res.json({ message: 'FUNCIONOU' }));
+        this.routes();
+    }
+    config() {
+        const accessControl = (_req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+            res.header('Access-Control-Allow-Headers', '*');
+            next();
+        };
+        this.app.use(express_1.default.json());
+        this.app.use(accessControl);
+    }
+    start(PORT) {
+        this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+    }
+    routes() {
+        this.app.use(routes_1.default);
+    }
+}
+exports.App = App;
+exports.app = new App().app;
---
+++ app/backend/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import router from './routes';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+
+    this.app.get('/', (req, res) => res.json({ message: 'FUNCIONOU' }));
+
+    this.routes();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+
+  private routes(): void {
+    this.app.use(router);
+  }
+}
+
+export { App };
+
+export const { app } = new App();
